bcf1430abc5b7b659fc1bf755c1ced66777a74e0
HepoHB <NeroHaziel@outlook.com>
2024-10-05 02:28:06 -0300

    Spring Project Created

---
+++ GerenciamentoDeRH/src/main/java/dev/kh/GerenciamentoDeRH/GerenciamentoDeRhApplication.java
@@ -0,0 +1,13 @@
+package dev.kh.GerenciamentoDeRH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeRhApplication.class, args);
+	}
+
+}
---
+++ GerenciamentoDeRH/src/test/java/dev/kh/GerenciamentoDeRH/GerenciamentoDeRhApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kh.GerenciamentoDeRH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
