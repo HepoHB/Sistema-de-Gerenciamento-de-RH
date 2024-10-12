f15037fd88ba6543704501cf9c8691c45130a736
HepoHB <NeroHaziel@outlook.com>
2024-10-12 00:36:34 -0300

    Docker Compose Working

--- venti/src/main/java/dev/KH/Venti/VentiApplication.java
+++ venti/src/main/java/dev/KH/Venti/VentiApplication.java
@@ -1,13 +1,13 @@
-package dev.kh.GerenciamentoDeRH;
+package dev.KH.Venti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerenciamentoDeRhApplication {
+public class VentiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GerenciamentoDeRhApplication.class, args);
+		SpringApplication.run(VentiApplication.class, args);
 	}
 
 }
--- venti/src/test/java/dev/KH/Venti/VentiApplicationTests.java
+++ venti/src/test/java/dev/KH/Venti/VentiApplicationTests.java
@@ -1,10 +1,10 @@
-package dev.kh.GerenciamentoDeRH;
+package dev.KH.Venti;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GerenciamentoDeRhApplicationTests {
+class VentiApplicationTests {
 
 	@Test
 	void contextLoads() {
